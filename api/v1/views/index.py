8950b3864b361c5a48a41a43527962040f4ec1e8
hug0-cstrs <hugo.casteras@outlook.fr>
2023-08-23 12:49:05 +0200

    5. Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,20 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """
+    returns class dictionary
+    """
+    cls_dict = {'amenities': storage.count('Amenity'),
+                'cities': storage.count('City'),
+                'places': storage.count('Place'),
+                'reviews': storage.count('Review'),
+                'states': storage.count('State'),
+                'users': storage.count('User')}
+    return jsonify(cls_dict)
